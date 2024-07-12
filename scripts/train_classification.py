b57df6d06cd05a03b414040fff7f95a07d1286bb
divya-kumari32 <divya.kumari32@gmail.com>
2024-07-12 11:18:08 -0400

    changed to fp32

--- scripts/train_classification.py
+++ scripts/train_classification.py
@@ -246,7 +246,7 @@ def training_state(model_path, model, rank):
 
 
 def main():
-    torch.set_default_dtype(torch.float16)
+    torch.set_default_dtype(torch.float32)
 
     print0("Loading model...")
     model = models.get_model(
